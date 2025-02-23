f14072b9d18345202beac003bde2d34343039b2a
Fernando <55415857+fernando-mendoza@users.noreply.github.com>
2025-02-22 19:32:20 -0600

    ultimas

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -37,14 +37,14 @@ export default function Newsletter() {
 
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Want more tutorials & guides?</h3>
-                <p className="text-gray-300 text-lg mb-6">Zulunity provides reliable DevSecOps solutions that seamlessly incorporate security practices into every phase of the software delivery lifecycle, ensuring not just protection, but resilience. Explore how we assist organizations with leveling up on security maturity levels.</p>
+                <h3 className="h3 text-white mb-2">Quieres saber mas?</h3>
+                <p className="text-gray-300 text-lg mb-6">Zulunity proporciona soluciones DevSecOps fiables que incorporan a la perfección prácticas de seguridad en cada fase del ciclo de vida de entrega de software, garantizando no solo la protección, sino la resiliencia. Descubra cómo ayudamos a las organizaciones a subir de nivel en los niveles de madurez de seguridad.</p>
 
                 {/* CTA form */}
                 <form className="w-full lg:w-auto">
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
                     <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribete</a>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
